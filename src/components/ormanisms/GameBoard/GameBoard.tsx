e995ef5b5bb27708d61cebc29aa6d0fac0159b66
tf-intern-koike <a.koike.p@p1.techfirm.co.jp>
2024-10-02 16:40:16 +0900

    インデントのサイズを修正

--- src/components/ormanisms/GameBoard/GameBoard.tsx
+++ src/components/ormanisms/GameBoard/GameBoard.tsx
@@ -2,45 +2,45 @@ import { GameState, convertMarkGameCols } from '@/components/templates/MarkGame/
 import style from '@/styles/GameBoard.module.css';
 
 type SquareProps = {
-    children: string,
-    onSquareClick: Function
+  children: string,
+  onSquareClick: Function
 }
 
 const Square: React.FC<SquareProps> = ({children, onSquareClick}) => {
-    return (
-        <td className={style.square} onClick={() => {onSquareClick();}}>
-            {children}
-        </td>
-    )
+  return (
+    <td className={style.square} onClick={() => {onSquareClick();}}>
+      {children}
+    </td>
+  )
 }
 
 export type GameBoardProps = {
-    gameState: GameState,
-    onGameBoardClick: (index: number) => void;
+  gameState: GameState,
+  onGameBoardClick: (index: number) => void;
 }
 
 export const GameBoard: React.FC<GameBoardProps> = ({gameState, onGameBoardClick}) => {
-    const boardWidth = gameState.boardWidth;
-    var boardData = gameState.boardData;
-    var cols = convertMarkGameCols(boardWidth, boardData);
+  const boardWidth = gameState.boardWidth;
+  var boardData = gameState.boardData;
+  var cols = convertMarkGameCols(boardWidth, boardData);
 
-    return <div>
-        <p className='desc'>GameBoard.tsx</p>
-        <table>
-        {
-        // 各行を出力する
-        cols.map((col, colIdx) =>
-            <tbody key={'board-tbody-' + colIdx}>
-            <tr key={'board-tr-' + colIdx}>
-            {
-            // 各行ごとの値を出力する
-            col.map((cell, rowIdx) =>
-                <Square key={'board-tr-' + colIdx * boardWidth + rowIdx} onSquareClick={
-                    () => {onGameBoardClick(colIdx * boardWidth + rowIdx);}}>{cell}</Square>
-            )}
-            </tr>
-            </tbody>
-        )}
-        </table>
-    </div>;
+  return <div>
+    <p className='desc'>GameBoard.tsx</p>
+    <table>
+    {
+    // 各行を出力する
+    cols.map((col, colIdx) =>
+      <tbody key={'board-tbody-' + colIdx}>
+      <tr key={'board-tr-' + colIdx}>
+      {
+      // 各行ごとの値を出力する
+      col.map((cell, rowIdx) =>
+        <Square key={'board-tr-' + colIdx * boardWidth + rowIdx} onSquareClick={
+          () => {onGameBoardClick(colIdx * boardWidth + rowIdx);}}>{cell}</Square>
+      )}
+      </tr>
+      </tbody>
+    )}
+    </table>
+  </div>;
 }
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
 import "@/styles/globals.css";
+import { MarkGameProvider } from "@/providers/MarkGameProvider";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <MarkGameProvider>
+    <Component {...pageProps} />
+  </MarkGameProvider>;
 }
